cfbb922dd870706acf0887ad14117ba6f3fc5c05
WadeStrong <info@wadestrong.com.au>
2026-04-19 23:04:33 +1200

    Create stripe-webhook.js

---
+++ netlify/functions/stripe-webhook.js
@@ -0,0 +1,4 @@
+const PRICE_MAP = {
+  'price_GET_STRONG_ID':  { type: 'get_strong',  weeks: 6,  label: 'Get Strong' },
+  'price_PRE_COMP_ID':    { type: 'pre_comp',    weeks: 8,  label: 'Pre-Comp Build Up' },
+};
